7b0d121d09c3b9d6c519ee9c3d82db7c3d473762
Jose Cagnini <jose.henriquec523@gmail.com>
2025-05-01 22:47:01 -0300

    feat: criado migrations e models

---
+++ app/models/reading_list.ts
@@ -0,0 +1,33 @@
+import type { BelongsTo, ManyToMany } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column, manyToMany } from '@adonisjs/lucid/orm'
+import User from './user.js'
+import Book from './book.js'
+
+export default class ReadingList extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare description: string | null
+
+  @column()
+  declare userId: number
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @manyToMany(() => Book, {
+    pivotTable: 'reading_list_books',
+  })
+  declare books: ManyToMany<typeof Book>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+}
--- database/migrations/1745983276572_create_user_table.ts
+++ database/migrations/1745983276572_create_user_table.ts
@@ -1,11 +1,11 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'users'
+  protected tableName = 'user'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
+      table.increments('id').primary().notNullable()
       table.string('full_name').nullable()
       table.string('login', 254).notNullable().unique()
       table.string('password').notNullable()
