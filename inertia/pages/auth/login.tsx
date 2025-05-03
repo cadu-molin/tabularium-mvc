7c7ba178f4057378a768f3a4e78b3246a0dc72d4
Jose Cagnini <jose.henriquec523@gmail.com>
2025-05-03 17:54:11 -0300

    fix: ajustado config do inertia

--- inertia/pages/auth/login.tsx
+++ inertia/pages/auth/login.tsx
@@ -1,16 +1,11 @@
 import { Head } from '@inertiajs/react'
-import { Button } from '~/components/ui/button'
 
 export default function Home() {
   return (
     <>
       <Head title="Registrar-se" />
 
-      <div className="">
-        teste
-        <input type="text" name="login" id="login" />
-      </div>
-      <Button>Click me</Button>
+      <div>registrar-se</div>
     </>
   )
 }
