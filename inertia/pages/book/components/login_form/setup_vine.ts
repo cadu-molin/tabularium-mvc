74b8468acb20b808ba7edd5c4d8193ccdfef68db
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2025-05-04 22:22:15 -0300

    Criação do Livro Parte 1

---
+++ inertia/pages/book/components/login_form/setup_vine.ts
@@ -0,0 +1,5 @@
+import { vineBookFormResolver, bookFormDefaultValues } from '#validators/book/book_validator'
+import type { BookFormSchema } from '#validators/book/book_validator'
+
+export { vineBookFormResolver, bookFormDefaultValues }
+export type { BookFormSchema }
--- start/routes.ts
+++ start/routes.ts
@@ -11,11 +11,13 @@ import router from '@adonisjs/core/services/router'
 import authRoute from './routes/auth_route.js'
 import homeRoute from './routes/home_route.js'
 import profileRoute from './routes/profile_route.js'
+import bookRoute from './routes/book_route.js'
 import { middleware } from './kernel.js'
 
 router.group(homeRoute(router)).as('home').use(middleware.userLogged())
 router.group(authRoute(router)).prefix('auth').as('auth').use(middleware.userLogged())
 router.group(profileRoute(router)).prefix('profile').as('profile').use(middleware.auth())
+router.group(bookRoute(router)).prefix('book').as('book').use(middleware.auth())
 
 router.get('*', ({ inertia }) => {
   return inertia.render('errors/not_found')
