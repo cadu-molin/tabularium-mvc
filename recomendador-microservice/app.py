acd7b494be35022bf78bddbfc660c2917b4cb319
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2025-07-08 21:38:20 -0300

    Parte 2

--- recomendador-microservice/app.py
+++ recomendador-microservice/app.py
@@ -82,4 +82,4 @@ def recomendar():
     return jsonify({"recomendacoes": recomendacoes})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='localhost', port=5001)
